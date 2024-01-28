3737654658b6a3256ba3d38555618b74985cfbbf
argicola <argicola.piger@yandex.ru>
2024-01-28 21:53:12 +0300

    chunk editor

--- src/app/editor/components/project/components/text-chunk/text-chunk.component.ts
+++ src/app/editor/components/project/components/text-chunk/text-chunk.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../../services/project.service';
 import { ChunkViewModel } from '../../../../models/chunkViewModel';
+import { IndexModel } from '../../../../models';
 
 @Component({
   selector: 'app-text-chunk',
@@ -11,11 +12,19 @@ export class TextChunkComponent implements OnInit {
 
   chunk?: ChunkViewModel;
   versions?: ChunkViewModel[];
+  index?: IndexModel;
 
   constructor(private projectService: ProjectService) {
     
   }
   ngOnInit(): void {
+
+    this.projectService.$currentIndex.subscribe(item=>{
+      if(item){
+        this.index = item;
+      }
+    })
+
     this.projectService.$currentChunk.subscribe(item=>{
       this.chunk = item;
       if(this.chunk){
