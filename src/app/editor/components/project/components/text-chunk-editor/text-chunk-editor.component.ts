47d399169bcaef3b087ac0209de3d8ffec78277e
aen-elle <klopkklopk@mail.ru>
2024-02-27 16:26:07 +0300

    chunk parser logic moved to server

--- src/app/editor/components/project/components/text-chunk-editor/text-chunk-editor.component.ts
+++ src/app/editor/components/project/components/text-chunk-editor/text-chunk-editor.component.ts
@@ -72,14 +72,7 @@ export class TextChunkEditorComponent extends BaseComponent implements OnInit {
 
     this.chunk.status = ChunkStatus.Published;
 
-    this.chunkParser.ParseTextToElements(this.chunk).then((items) => {
-      let elements = items;
-
-      let lang = this.projectService.$currentHeader.value?.lang;
-
-      let chunkValueItems: ChunkValueItemModel[] = [];
-
-      let clearChunk = new ChunkModel({
+    let clearChunk = new ChunkModel({
         _id: this.chunk._id,
         created: this.chunk.created,
         headerId: this.chunk.headerId,
@@ -89,102 +82,37 @@ export class TextChunkEditorComponent extends BaseComponent implements OnInit {
         value: this.chunk.value,
       });
 
-      if (lang)
-        this.projectService
-          .GetMorphItems(lang)
-          .then((ruleItems) => {
-            elements.forEach((element) => {
-              let chunkValueItem = new ChunkValueItemModel({
-                value: element.value,
-                type: element.type,
-                order: element.order,
-              });
-
-              let rules = ruleItems.filter(
-                (i) =>
-                  i.form?.toLocaleLowerCase() ==
-                  element.value?.toLocaleLowerCase()
-              );
-
-              if (rules.length == 1) {
-                let morphRule = rules[0];
-                element.morphId = morphRule._id;
-                chunkValueItem.morphId = morphRule._id;
-                chunkValueItem.case = morphRule.case;
-                chunkValueItem.degree = morphRule.degree;
-                chunkValueItem.dialect = morphRule.dialect;
-                chunkValueItem.feature = morphRule.feature;
-                chunkValueItem.form = morphRule.form;
-                chunkValueItem.gender = morphRule.gender;
-                chunkValueItem.lang = morphRule.lang;
-                chunkValueItem.lemma = morphRule.lemma;
-                chunkValueItem.number = morphRule.number;
-                chunkValueItem.mood = morphRule.mood;
-                chunkValueItem.person = morphRule.person;
-                chunkValueItem.pos = morphRule.pos;
-                chunkValueItem.tense = morphRule.tense;
-                chunkValueItem.voice = morphRule.voice;
-              }
-              chunkValueItems.push(chunkValueItem);
-            });
-
-            clearChunk.valueObj = JSON.stringify(chunkValueItems);
-          })
-          .then(() => {
-            this.projectService
-              .SaveChunk(clearChunk)
-              .then((savedChunk) => {
-                let sch = savedChunk as ChunkModel;
-                if (sch && sch._id) {
-                  this.projectService
-                    .DeleteElementsByChunk(sch._id)
-                    .then(() => {
-                      elements.forEach((el) => {
-                        el.chunkId = sch._id;
-                        el.headerId = sch.headerId;
-                        this.projectService.SaveElement(el);
-                      });
-                    });
-                }
-                return sch;
-              })
-              .then((sch) => {
-                if (sch.indexId) {
-                  if (
-                    this.projectService.$currentHeader.value &&
-                    this.projectService.$currentHeader.value.editionType ==
-                      EditionType.Interpretation
-                  ) {
-                    //TODO:Check if the link is already exists
-                    this.projectService
-                      .GetChunkByQuery(
-                        new ChunkQuery({
-                          indexName:
-                            this.projectService.$currentIndex.value?.name,
-                            headerId: this.projectService.$projectHeaders.value?.find(i=>i.editionType == EditionType.Original)?._id
-                        })
-                      )
-                      .then((result) => {
-                        if (result) {
-                          let interpModel = new InterpModel({
-                            sourceId: result[0]._id,
-                            sourceHeaderId: result[0].headerId,
-                            interpId: sch._id,
-                            interpHeaderId: sch.headerId,
-                          });
-                          this.projectService.SaveInterpLink(interpModel);
-                        }
-                      });
-                    this.projectService.GetChunk(sch.indexId);
-                  }
+      this.projectService.SaveChunk(clearChunk).then((savedChunk) => {
+        let sch = savedChunk as ChunkModel;
+        if (sch.indexId) {
+          if (
+            this.projectService.$currentHeader.value &&
+            this.projectService.$currentHeader.value.editionType == EditionType.Interpretation) {
+              this.projectService.GetChunkByQuery(
+                new ChunkQuery({
+                  indexName:
+                    this.projectService.$currentIndex.value?.name,
+                  headerId: this.projectService.$projectHeaders.value?.find(i => i.editionType == EditionType.Original)?._id
+                })
+              ).then((result) => {
+                if (result) {
+                  let interpModel = new InterpModel({
+                    sourceId: result[0]._id,
+                    sourceHeaderId: result[0].headerId,
+                    interpId: sch._id,
+                    interpHeaderId: sch.headerId,
+                  });
+                  this.projectService.SaveInterpLink(interpModel);
                 }
               });
-          })
-          .finally(() => {
-            this.uiService.$progressBarIsOn.next(false);
-          });
-    });
-
+            }
+          this.projectService.GetChunk(sch.indexId);  
+          }
+        }).finally(() => {
+          this.uiService.$progressBarIsOn.next(false);
+        });
+        
+    
     this.dialogRef.close();
   }
 
