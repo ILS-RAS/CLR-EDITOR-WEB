ad3c72f8308f6d4dd7f333f3cdf7b2c35bec3343
aen-elle <klopkklopk@mail.ru>
2024-05-07 16:04:02 +0300

    taxonomy page

--- src/app/editor/components/taxonomy/components/default/default.component.ts
+++ src/app/editor/components/taxonomy/components/default/default.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaxonomyViewModel } from '../../../../models';
+import { BaseComponent } from '../../../../../components/base/base/base.component';
+import { MetaService } from '../../../project/services/meta.service';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-default',
@@ -7,4 +11,7 @@ import { Component } from '@angular/core';
 })
 export class DefaultComponent {
 
+createTaxonomy(): void {
+  throw new Error('Method not implemented.')
+}
 }
